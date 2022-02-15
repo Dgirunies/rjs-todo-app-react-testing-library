bcddf577ef10b3d930d0f48a5294fd8221c3d7c0
Dgirunies <erineu16@gmail.com>
2022-02-15 19:14:51 +0100

    Unity tests by title and testId

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -7,14 +7,26 @@ test("should render same text passed into title prop", () => {
   expect(headingElement).toBeInTheDocument();
 });
 
-test("should render a heading role", () => {
+test("should render an heading role", () => {
   render(<Header />);
   const headingElement = screen.getByRole("heading");
   expect(headingElement).toBeInTheDocument();
 });
 
-test("should render a heading role with the text passed on title prop", () => {
+// test("should render an heading role with the text passed on title prop", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByRole("heading", { name: "My Header" });
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+test("should render an h1 role with the title `Header` on it", () => {
   render(<Header title="My Header" />);
-  const headingElement = screen.getByRole("heading", { name: "My Header" });
+  const headingElement = screen.getByTitle("Header");
+  expect(headingElement).toBeInTheDocument();
+});
+
+test("should render an h1 role with a test id of `header-1` on it", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTestId("header-1");
   expect(headingElement).toBeInTheDocument();
 });
