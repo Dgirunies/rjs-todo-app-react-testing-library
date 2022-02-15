4a74f190f48ded84aff8eeacbbaf0c8f43353096
Dgirunies <erineu16@gmail.com>
2022-02-15 18:46:55 +0100

    Testing the Header geting by checking both the heading role and the title

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,10 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
 
-test('should render same text passed into title prop', () => {
-  render(<Header title="My Header"/>);
-  const headingElement = screen. getByText(/My header/i);
+test("should render same text passed into title prop", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByText(/My header/i);
   expect(headingElement).toBeInTheDocument();
 });
 
+test("should render a heading role", () => {
+  render(<Header />);
+  const headingElement = screen.getByRole("heading");
+  expect(headingElement).toBeInTheDocument();
+});
 
+test("should render a heading role with the text passed on title prop", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByRole("heading", { name: "My Header" });
+  expect(headingElement).toBeInTheDocument();
+});
