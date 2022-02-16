8f1a1ef372bb64cd9165aacbfe4a3a94fe228e43
Dgirunies <erineu16@gmail.com>
2022-02-16 16:43:42 +0100

    Having a look in assertions

--- src/components/TodoFooter/__tests__/TodoFooter.test.js
+++ src/components/TodoFooter/__tests__/TodoFooter.test.js
@@ -15,3 +15,40 @@ test("should render the correct amount of incomplete tastks", () => {
   const paragraphElement = screen.getByText(/5 tasks left/i);
   expect(paragraphElement).toBeInTheDocument();
 });
+
+test("should render 'task' when the number of incomplete tasks is one", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+test("should render 'task' when the number of incomplete tasks is one checking if it is truphy", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeTruthy();
+});
+
+test("should render 'task' when the number of incomplete tasks is one checking if it is visible", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeVisible();
+});
+
+test("should render 'task' when the number of incomplete tasks is one checking if there is a p tag", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toContainHTML("p");
+});
+
+test("should render 'task' when the number of incomplete tasks is one checking if the correct text content", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("para");
+  expect(paragraphElement).toHaveTextContent("1 task left");
+});
+
+test("should render 'task' when the number of incomplete tasks by getting an element attribute", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("para");
+  expect(paragraphElement.textContent).toBe("1 task left");
+  expect(paragraphElement.textContent).toBe("1 task left");
+});
