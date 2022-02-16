bb75d817acc5f2d21ebfa8e0dec08cc28554d649
Dgirunies <erineu16@gmail.com>
2022-02-16 16:34:06 +0100

    Using a Mock component in tests

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -7,11 +7,11 @@ test("should render same text passed into title prop", () => {
   expect(headingElement).toBeInTheDocument();
 });
 
-test("should render an heading role", () => {
-  render(<Header />);
-  const headingElement = screen.getByRole("heading");
-  expect(headingElement).toBeInTheDocument();
-});
+// test("should render an heading role", () => {
+//   render(<Header />);
+//   const headingElement = screen.getByRole("heading");
+//   expect(headingElement).toBeInTheDocument();
+// });
 
 // test("should render an heading role with the text passed on title prop", () => {
 //   render(<Header title="My Header" />);
@@ -19,36 +19,36 @@ test("should render an heading role", () => {
 //   expect(headingElement).toBeInTheDocument();
 // });
 
-test("should render an h1 role with the title `Header` on it", () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.getByTitle("Header");
-  expect(headingElement).toBeInTheDocument();
-});
+// test("should render an h1 role with the title `Header` on it", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByTitle("Header");
+//   expect(headingElement).toBeInTheDocument();
+// });
 
-test("should render an h1 role with a test id of `header-1` on it", () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.getByTestId("header-1");
-  expect(headingElement).toBeInTheDocument();
-});
+// test("should render an h1 role with a test id of `header-1` on it", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.getByTestId("header-1");
+//   expect(headingElement).toBeInTheDocument();
+// });
 
-// FIND BY
+// // FIND BY
 
-test("should render same text passed into title prop (using findBy)", async () => {
-  render(<Header title="My Header" />);
-  const headingElement = await screen.findByText(/My header/i);
-  expect(headingElement).toBeInTheDocument();
-});
+// test("should render same text passed into title prop (using findBy)", async () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = await screen.findByText(/My header/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
 
-// Query By
+// // Query By
 
-test("expects not to find a dog text in the document", () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.queryByText(/dog/i);
-  expect(headingElement).not.toBeInTheDocument();
-});
+// test("expects not to find a dog text in the document", () => {
+//   render(<Header title="My Header" />);
+//   const headingElement = screen.queryByText(/dog/i);
+//   expect(headingElement).not.toBeInTheDocument();
+// });
 
-test("expects not to find a dog text in the document", () => {
-  render(<Header title="My Header" />);
-  const headingElements = screen.getAllByRole("heading");
-  expect(headingElements.length).toBe(1);
-});
+// test("expects not to find a dog text in the document", () => {
+//   render(<Header title="My Header" />);
+//   const headingElements = screen.getAllByRole("heading");
+//   expect(headingElements.length).toBe(1);
+// });
